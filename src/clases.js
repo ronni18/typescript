cf630892460aa6a473e8e16a7a2bf70d55ae7cfc
roni18 <ronnicarrascaltejedor@gmail.com>
2021-07-22 22:57:17 -0500

    se agregaron metodos get y set para acceder y modificar variables privadas de la clase, una super clase item

--- src/clases.js
+++ src/clases.js
@@ -1,4 +1,17 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var PhotoOrientation;
 (function (PhotoOrientation) {
@@ -7,35 +20,82 @@ var PhotoOrientation;
     PhotoOrientation[PhotoOrientation["Square"] = 2] = "Square";
     PhotoOrientation[PhotoOrientation["Panorama"] = 3] = "Panorama";
 })(PhotoOrientation || (PhotoOrientation = {}));
-var Picture = /** @class */ (function () {
+//super clase
+var Item = /** @class */ (function () {
+    function Item(id, title) {
+        this._id = id;
+        this._title = title;
+    }
+    Object.defineProperty(Item.prototype, "id", {
+        //accediendo a las variables por medio de get
+        //dando valores a las variables por medio del metodo set
+        get: function () {
+            return this._id;
+        },
+        set: function (id) {
+            this._id = id;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Object.defineProperty(Item.prototype, "title", {
+        get: function () {
+            return this._title;
+        },
+        set: function (title) {
+            this._title = title;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    return Item;
+}());
+var Picture = /** @class */ (function (_super) {
+    __extends(Picture, _super);
     function Picture(id, title, orientation) {
-        this.id = id;
-        this.title = title;
-        this.orientation = orientation;
+        var _this = _super.call(this, id, title) || this;
+        _this._orientation = orientation;
+        return _this;
     }
     //comportamiento
     Picture.prototype.toString = function () {
-        return "[id: " + this.id + ",\n                title: " + this.title + ",\n                orientation: " + this.orientation + "]";
+        return "[id: " + this.id + ",\n                title: " + this.title + ",\n                orientation: " + this._orientation + "]";
     };
+    Object.defineProperty(Picture.prototype, "orientation", {
+        get: function () {
+            return this._orientation;
+        },
+        set: function (orientation) {
+            this._orientation = orientation;
+        },
+        enumerable: true,
+        configurable: true
+    });
+    Picture.photoOrientation = PhotoOrientation;
     return Picture;
-}());
-var Album = /** @class */ (function () {
+}(Item));
+var Album = /** @class */ (function (_super) {
+    __extends(Album, _super);
     function Album(id, title) {
-        this.id = id;
-        this.title = title;
-        this.pictures = [];
+        var _this = _super.call(this, id, title) || this;
+        _this.pictures = [];
+        return _this;
     }
     Album.prototype.addPicture = function (picture) {
         this.pictures.push(picture);
     };
     return Album;
-}());
-var album = new Album(1, 'personal picture');
+}(Item));
+var album = new Album(1, 'local picture');
 var picture = new Picture(1, 'roni personal', PhotoOrientation.Square);
 album.addPicture(picture);
 console.log(album);
 //accediendo a los miembros publicos
-//picture.id = 100;
-///picture.title = 'nuevo titulo';
-///album.title = 'nuevo album';
-//console.log(album);
+console.log('picture.id ', picture.id); //get id()
+picture.id = 100; // private , set id(100)
+picture.title = 'nuevo titulo'; //private 
+album.title = 'nuevo album'; //private
+console.log(album);
+//const item = new Item(1, 'new title'); //no se puede crear un objeto debido a que es una clase abstracta
+//console.log('new item',item);
+console.log('photoOrientation : ', Picture.photoOrientation.Portrait);
--- src/clases.ts
+++ src/clases.ts
@@ -7,20 +7,55 @@ enum PhotoOrientation{
     Panorama
 }
 
- class Picture {
+//super clase
+
+abstract class Item { //se declara clase abstracta para que no se creen objetos con ella
+
+    //propiedades
+
+    protected _id: number;
+    protected _title: string;
+
+    public  constructor(id: number, title: string) {
+        this._id = id;
+        this._title = title;        
+    }
+
+    //accediendo a las variables por medio de get
+    //dando valores a las variables por medio del metodo set
+
+    get id() {
+        return this._id;
+    }
+    
+    set id(id : number) {
+        this._id = id;
+    }
+    get title() {
+        return this._title;
+    }
+    
+    set title(title : string) {
+        this._title = title;
+    }
+    
+    
+}
 
 
-   private id:number;
-   private title:string;
-   private orientation: PhotoOrientation;
+ class Picture extends Item{
+     public static photoOrientation = PhotoOrientation;
+
+    //propiedades
+
+   private _orientation: PhotoOrientation;
   
 
    public constructor(id:number,
                 title:string,
                 orientation: PhotoOrientation) {
-                    this.id = id;
-                    this.title= title;
-                    this.orientation =orientation;
+                    super(id,title); // constructor de super clase
+                    this._orientation =orientation;
                 
     }
 
@@ -29,24 +64,27 @@ enum PhotoOrientation{
    public toString (){
         return `[id: ${this.id},
                 title: ${this.title},
-                orientation: ${this.orientation}]`
+                orientation: ${this._orientation}]`
 
     }
 
-
+    get orientation() {
+        return this._orientation;
+    }
+    
+    public set orientation(orientation : PhotoOrientation) {
+        this._orientation = orientation;
+    }
+    
 }
 
-class Album {
+class Album extends Item{
 
-
-    private id:number;
-    private title:string;
     private pictures: Picture[];
 
 
    public constructor(  id: number, title: string ) {
-        this.id = id;
-        this.title = title;
+       super(id,title); // constructor de super clase
         this.pictures = [];
         
     }
@@ -54,20 +92,24 @@ class Album {
   public addPicture( picture: Picture) {
         this.pictures.push( picture );
     }
+    
 }
 
-const album: Album = new Album(1, 'personal picture');
+const album: Album = new Album(1, 'local picture');
 const picture: Picture = new Picture(1, 'roni personal', PhotoOrientation.Square);
-
 album.addPicture( picture );
-
 console.log(album);
 
 
 //accediendo a los miembros publicos
 
-//picture.id = 100;
-///picture.title = 'nuevo titulo';
-///album.title = 'nuevo album';
+console.log('picture.id ' , picture.id) //get id()
+picture.id = 100; // private , set id(100)
+picture.title = 'nuevo titulo'; //private 
+album.title = 'nuevo album'; //private
+console.log(album);
 
-//console.log(album);
+//const item = new Item(1, 'new title'); //no se puede crear un objeto debido a que es una clase abstracta
+//console.log('new item',item);
+
+console.log('photoOrientation : ',Picture.photoOrientation.Portrait);
